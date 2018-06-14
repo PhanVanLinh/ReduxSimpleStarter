4814227f3d2e346907f1c3f20144903676851600
PhanVanLinh <phanvanlinh.94vn@gmail.com>
2018-06-14 13:09:15 +0700

    add demo actions

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,29 +1,40 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
 
-    renderList(){
+    renderList() {
         return this.props.books.map((book => {
-            return(
-                <li key={book.title} className="list-group-item">{book.title}</li>
+            return (
+                <li
+                    key={book.title}
+                    className="list-group-item"
+                    onClick={() => this.props.selectBook(book)}>
+                    {book.title}
+                </li>
             );
         }));
     }
 
-    render(){
+    render() {
         return (
             <ul className="list-group col-sm-4">
-            {this.renderList()}
+                {this.renderList()}
             </ul>
         )
     }
 }
 
-function mapStateToProp(state){
+function mapStateToProp(state) {
     return {
         books: state.books
     };
 }
 
-export default connect(mapStateToProp)(BookList);
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
+
+export default connect(mapStateToProp, mapDispatchToProps)(BookList);
